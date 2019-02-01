588650dd1f5c5cf8556959a59903709a9ef46f14
DTE <w.figo.1991@gmail.com>
2019-02-01 12:42:34 -0500

    organizacion

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -8,11 +8,11 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Inicio',  icon: 'design_app', class: '' },
-    { path: '/organizaciones', title: 'Organizacion',  icon: 'users_single-02', class: '' },
+    { path: '/organizaciones', title: 'Organizacion',  icon: 'business_bank', class: '' },
     { path: '/miembros', title: 'Miembros',  icon: 'users_single-02', class: '' },
-    { path: '/eventos', title: 'Eventos',  icon: 'users_single-02', class: '' },
+    { path: '/eventos', title: 'Eventos',  icon: 'ui-1_calendar-60', class: '' },
     { path: '/icons', title: 'Miembros',  icon: 'education_atom', class: '' },
-     { path: '/user-profile', title: 'Users',  icon: 'location_map-big', class: '' },
+    { path: '/user-profile', title: 'Users',  icon: 'location_map-big', class: '' },
     { path: '/notifications', title: 'Notifications',  icon: 'ui-1_bell-53', class: '' },
     { path: '/table-list', title: 'Table List',  icon: 'design_bullet-list-67', class: '' },
     { path: '/typography', title: 'Typography',  icon: 'text_caps-small', class: '' }
