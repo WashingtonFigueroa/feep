9cb673d916ef870a2b0549cb6bab514b268b9dd7
DTE <w.figo.1991@gmail.com>
2019-02-01 12:34:56 -0500

    organizacion

--- frontend/src/app/organizacion/organizacion-index/organizacion-index.component.ts
+++ frontend/src/app/organizacion/organizacion-index/organizacion-index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from '../../../environments/environment.prod';
+import {OrganizacionService} from '../organizacion.service';
 
 @Component({
   selector: 'app-organizacion-index',
@@ -7,9 +9,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizacionIndexComponent implements OnInit {
 
-  constructor() { }
+    prev_page: string = null;
+    next_page: string = null;
+    last_page: number = null;
+    pages: any = [];
+    current_page: any = null;
+    organizaciones: any = null;
+    url_base = environment.servidor + 'organizaciones-imagen/'
+  valor = '';
+    constructor(private organizacionService: OrganizacionService) {
+        this.organizacionService.index()
+            .subscribe((res: any) => {
+                this.organizaciones = res;
+                console.log(res);
+                this.current_page = this.organizaciones.current_page;
+                this.prev_page = this.organizaciones.prev_page_url;
+                this.next_page = this.organizaciones.next_page_url;
+                this.last_page = this.organizaciones.last_page;
+                this.loadPages();
+            });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    loadPages() {
+        this.pages = [];
+        for (let i = 1; i <= this.last_page; i++) {
+            this.pages.push({
+                item: i,
+                url: environment.servidor + 'organizaciones?page=' + i
+            });
+        }
+    }
+
+    loadPagination(url: string) {
+        this.organizacionService.pagination(url)
+            .subscribe((res: any) => {
+                this.organizaciones = res;
+                this.current_page = this.organizaciones.current_page;
+                this.prev_page = this.organizaciones.prev_page_url;
+                this.next_page = this.organizaciones.next_page_url;
+                this.last_page = this.organizaciones.last_page;
+                this.loadPages();
+            });
+    }
+
+    buscar(valor: string) {
+        this.organizacionService.buscar(valor)
+            .subscribe((res: any) => {
+                this.organizaciones = res;
+                this.current_page = this.organizaciones.current_page;
+                this.prev_page = this.organizaciones.prev_page_url;
+                this.next_page = this.organizaciones.next_page_url;
+                this.last_page = this.organizaciones.last_page;
+                this.loadPages();
+            });
+    }
+    destroy(organizacion, index) {
+        if (confirm('Esta seguro de eliminar al organizacion ' + organizacion.nombre)) {
+            this.organizacionService.destroy(organizacion.organizacion_id)
+                .subscribe((res: any) => {
+                    this.organizaciones.data.splice(index, 1);
+                });
+        }
+    }
 }
