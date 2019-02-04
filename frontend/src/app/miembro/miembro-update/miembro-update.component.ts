0784cc87a4554a1e43897db077a29a014fc7abda
DTE <w.figo.1991@gmail.com>
2019-02-04 13:00:19 -0500

    update

--- frontend/src/app/miembro/miembro-update/miembro-update.component.ts
+++ frontend/src/app/miembro/miembro-update/miembro-update.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
+import {MiembroService} from "../miembro.service";
+import {TipoPersonaService} from "../../tipo-persona/tipo-persona.service";
+import {OrganizacionService} from "../../organizacion/organizacion.service";
+import {UbicacionService} from "../../ubicacion/ubicacion.service";
 
 @Component({
   selector: 'app-miembro-update',
@@ -7,9 +14,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiembroUpdateComponent implements OnInit {
 
-  constructor() { }
+    persona_id: number = null;
+    persona: any = null;
+    personaGroup: FormGroup;
+    tipo_personas : any = null;
+    organizaciones: any = null;
+    parroquias: any = null;
+    constructor(private personaService: MiembroService,
+                private tipopersonaService: TipoPersonaService,
+                private organizaconService: OrganizacionService,
+                private parroquiasiService: UbicacionService,
+                private fb: FormBuilder,
+                private router: Router,
+                private route: ActivatedRoute,
+                private toastrService:ToastrService) {
+        this.tipopersonaService.listar()
+            .subscribe((res: any) => {
+                this.tipo_personas = res;
+            });
+        this.organizaconService.listar()
+            .subscribe((res: any) => {
+                this.organizaciones = res;
+            });
+        this.parroquiasiService.parroquiaslistar()
+            .subscribe((res: any) => {
+                this.parroquias = res;
+            });
+        this.route.params.subscribe((param: any) => {
+            this.persona_id = param.id;
+            this.personaService.show(this.persona_id)
+                .subscribe((res: any) => {
+                    this.persona = res;
+                    this.crearForm();
+                });
+        });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    crearForm() {
+        this.personaGroup = this.fb.group({
+            'tipo_persona_id': [this.persona.tipo_persona_id, [Validators.required]],
+            'organizacion_id': [this.persona.organizacion_id, [Validators.required]],
+            'parroquia_id': [this.persona.parroquia_id, [Validators.required]],
+            'cedula': [this.persona.cedula, [Validators.required]],
+            'nombres': [this.persona.nombres, [Validators.required]],
+            'genero': [this.persona.genero, [Validators.required]],
+            'ocupacion': [this.persona.ocupacion, [Validators.required]],
+            'etnia': [this.persona.etnia, [Validators.required]],
+            'fecha_nacimiento': [this.persona.fecha_nacimiento, [Validators.required]],
+            'direccion': [this.persona.direccion, [Validators.required]],
+            'telefono_fijo': [this.persona.telefono_fijo],
+            'operadora': [this.persona.operadora, [Validators.required]],
+            'contacto': [this.persona.contacto],
+            'email': [this.persona.email],
+        });
+    }
+
+    update() {
+        this.personaService.update(this.persona_id, this.personaGroup.value)
+            .subscribe((res: any) => {
+              this.toastrService.success('Datos Modificados','Persona');
+                this.router.navigate(['/miembros']);
+            });
+    }
 }
