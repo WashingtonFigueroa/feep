abd37a81e53b73effd87887c749527549de22e5e
DTE <w.figo.1991@gmail.com>
2019-03-14 22:43:41 -0500

    correccion1

--- frontend/src/app/organizacion/organizacion-create/organizacion-create.component.ts
+++ frontend/src/app/organizacion/organizacion-create/organizacion-create.component.ts
@@ -41,11 +41,15 @@ export class OrganizacionCreateComponent implements OnInit {
             'actividad_id': [0, [Validators.required]],
             'documento': ['', [Validators.required]],
             'nombre': ['', [Validators.required]],
+            'caracteristica1': ['', [Validators.required]],
+            'caracteristica2': ['', [Validators.required]],
+            'caracteristica3': ['', [Validators.required]],
             'imagen': [''],
             'representante': ['', [Validators.required]],
             'contacto': [''],
             'direccion': [''],
             'descripcion': [''],
+            'ministerio': [''],
             'acuerdo': [''],
             'mujeres': [''],
             'ninias': [''],
@@ -71,11 +75,15 @@ export class OrganizacionCreateComponent implements OnInit {
             formData.append('actividad_id', this.organizacionGroup.value.actividad_id);
             formData.append('documento', this.organizacionGroup.value.documento);
             formData.append('nombre', this.organizacionGroup.value.nombre.toUpperCase());
+            formData.append('caracteristica1', this.organizacionGroup.value.caracteristica1);
+            formData.append('caracteristica2', this.organizacionGroup.value.caracteristica2);
+            formData.append('caracteristica3', this.organizacionGroup.value.caracteristica3);
             formData.append('actividad', this.organizacionGroup.value.actividad);
             formData.append('representante', this.organizacionGroup.value.representante.toUpperCase());
             formData.append('contacto', this.organizacionGroup.value.contacto);
             formData.append('direccion', this.organizacionGroup.value.direccion);
             formData.append('descripcion', this.organizacionGroup.value.descripcion.toUpperCase());
+            formData.append('ministerio', this.organizacionGroup.value.ministerio);
             formData.append('acuerdo', this.organizacionGroup.value.acuerdo);
             formData.append('mujeres', this.organizacionGroup.value.mujeres);
             formData.append('ninias', this.organizacionGroup.value.ninias);
