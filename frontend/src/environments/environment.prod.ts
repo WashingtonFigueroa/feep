03d1ecef5dc72754636c8f67b391b7cfc2c782e5
DTE <w.figo.1991@gmail.com>
2019-06-28 09:50:16 -0500

    actualizacion

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  servidor2: 'http://fepp.dte-ecuador.org/public/api/v1/',
-  servidor: 'http://localhost:8000/api/v1/'
+  servidor: 'https://www.sisevents.dtmowed.org/public/api/v1/',
+  servidor2: 'http://localhost:8000/api/v1/'
 };
